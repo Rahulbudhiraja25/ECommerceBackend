4b6dcae12a7b9cd7fe3c62ba1860dcda6b875e5e
RahulBudhiraja <91089279+Rahulbudhiraja25@users.noreply.github.com>
2025-10-18 17:38:44 +0530

    Initialized a project Ecommerce-Backend

---
+++ src/test/java/com/learn/ECommerceBackend/ECommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.ECommerceBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
